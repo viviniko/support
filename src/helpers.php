52719957b20335424e8e9ee5d321290eb5d5a3c2
viviniko <viviniko.shop@gmail.com>
2018-05-07 15:58:43 +0800

    :trollface:

--- src/helpers.php
+++ src/helpers.php
@@ -14,7 +14,7 @@ if (!function_exists('price_format')) {
      */
     function price_format($price, $decimals = null, $decimalPoint = null, $thousandsSep = null)
     {
-        return number_format($price, $decimals ?: 2, $decimalPoint ?: '.', is_null($thousandsSep) ? ',' : $thousandsSep);
+        return !is_numeric($price) ? $price : number_format($price, $decimals ?: 2, $decimalPoint ?: '.', is_null($thousandsSep) ? ',' : $thousandsSep);
     }
 }
 
